088bce8738da81d2a0a358451ea7cc4d161ec319
Wolfmaster30306 <69368148+Wolfmaster30306@users.noreply.github.com>
2021-08-14 12:39:47 -0600

    Added tiles and tileset

---
+++ Tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="11">
+  <image width="64" height="64" source="tiles/grass.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="tiles/rock.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="tiles/water.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="tiles/waterbed.png"/>
+ </tile>
+</tileset>
