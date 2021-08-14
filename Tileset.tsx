3009906d7823a1f7bc2b6fea2fae5fe5511fc5c5
Wolfmaster30306 <69368148+Wolfmaster30306@users.noreply.github.com>
2021-08-14 12:47:16 -0600

    Added crate tile

--- Tileset.tsx
+++ Tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="11">
   <image width="64" height="64" source="tiles/grass.png"/>
@@ -12,5 +12,8 @@
  </tile>
  <tile id="14">
   <image width="64" height="64" source="tiles/waterbed.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="tiles/Crate.png"/>
  </tile>
 </tileset>
