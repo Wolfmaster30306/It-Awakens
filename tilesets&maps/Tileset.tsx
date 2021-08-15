b4ba5b15c605a6dd7f38a98448d49a987fd80460
Wolfmaster30306 <hopwaidi@gmail.com>
2021-08-15 14:05:32 -0600

    Update Tileset.tsx

--- tilesets&maps/Tileset.tsx
+++ tilesets&maps/Tileset.tsx
@@ -1,19 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+<tileset version="1.5" tiledversion="1.7.1" name="Tileset" tilewidth="64" tileheight="64" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="11">
-  <image width="64" height="64" source="tiles/grass.png"/>
+ <tile id="16">
+  <image width="64" height="64" source="../tiles/Crate.png"/>
  </tile>
- <tile id="12">
-  <image width="64" height="64" source="tiles/rock.png"/>
+ <tile id="17">
+  <image width="64" height="64" source="../tiles/grass.png"/>
  </tile>
- <tile id="13">
-  <image width="64" height="64" source="tiles/water.png"/>
+ <tile id="18">
+  <image width="64" height="64" source="../tiles/rock.png"/>
  </tile>
- <tile id="14">
-  <image width="64" height="64" source="tiles/waterbed.png"/>
+ <tile id="19">
+  <image width="64" height="64" source="../tiles/water.png"/>
  </tile>
- <tile id="15">
-  <image width="64" height="64" source="tiles/Crate.png"/>
+ <tile id="20">
+  <image width="64" height="64" source="../tiles/waterbed.png"/>
  </tile>
 </tileset>
